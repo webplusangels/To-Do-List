02a6b6ff9e4153d4945c0f988227efaa54970604
webplusangels <nimweaver14@gmail.com>
2023-10-20 23:40:04 +0900

    Initialize Project
    - Add README.md
    - Add .gitignore
    - Add server.js to test

---
+++ server.js
@@ -0,0 +1,18 @@
+// express 불러오기
+const express = require("express");
+
+// express 사용
+const app = express();
+
+// 포트 번호 설정
+const port = 5000;
+
+// http 서버 실행
+app.listen(port,() =>{
+    console.log("서버가 정상적으로 실행되었습니다.");
+});
+
+// http://localhost:5000/ 경로로 접근
+app.get("/",(request,response)=>{
+    response.send("수정되었습니다.");
+})
